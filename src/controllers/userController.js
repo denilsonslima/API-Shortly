2c1c3b2b9268d129c0a8e55b39b2fd1957da62db
Denilson Lima <denilsondslima@gmail.com>
2023-03-03 00:40:05 -0300

    feat: rota delete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,3 +25,29 @@ export const dadosUser = async (req, res) => {
 
     res.send(dados.rows[0])
 }
+
+export const deletarUsuario = async (req, res) => {
+    const token = res.locals.token;
+    const id = req.params.id
+
+    const url = await db.query(`
+    SELECT * FROM urls WHERE id = $1
+    `, [id])
+
+    if(url.rowCount === 0) return res.sendStatus(404)
+
+    const dados = await db.query(`
+    SELECT * FROM urls AS u
+    JOIN sessions AS s
+        ON u."userId" = s."userId"
+    WHERE s.token = $1 AND u.id = $2 
+    `, [token, id])
+
+    if(dados.rowCount === 0) return res.sendStatus(401)
+
+    await db.query(`
+    DELETE * FROM urls WHERE id = $1
+    `, [id])
+
+    res.sendStatus(204)
+}
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { dadosUser } from '../controllers/userController.js'
+import { dadosUser, deletarUsuario } from '../controllers/userController.js'
 import { authValidate } from '../middlewares/authMiddlewares.js'
 
 const userRouter = Router()
 
 userRouter.get("/users/me", authValidate, dadosUser)
+userRouter.delete("/urls/:id", authValidate, deletarUsuario)
 
 export default userRouter
