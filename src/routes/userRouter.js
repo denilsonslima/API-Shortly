a6ccdafa9192d862031d3b5f9412e08bfb5dfd58
Denilson Lima <denilsondslima@gmail.com>
2023-03-03 02:24:19 -0300

    fix: ranking

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { dadosUser, deletarUsuario } from '../controllers/userController.js'
+import { dadosUser, deletarUsuario, ranking } from '../controllers/userController.js'
 import { authValidate } from '../middlewares/authMiddlewares.js'
 
 const userRouter = Router()
 
 userRouter.get("/users/me", authValidate, dadosUser)
 userRouter.delete("/urls/:id", authValidate, deletarUsuario)
+userRouter.get("/ranking", ranking)
 
 export default userRouter
